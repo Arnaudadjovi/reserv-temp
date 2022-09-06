68fcb93512781b1773d2a0ef28d48aee482e9920
Arnaudadjovi <103598812+Arnaudadjovi@users.noreply.github.com>
2022-09-06 18:16:13 +0100

    Initialize zeloide/modif

---
+++ database/migrations/2022_04_26_211709_create_comptes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comptes', function (Blueprint $table) {
+            $table->id();
+            $table->string("email");
+            $table->string("password");
+            $table->timestamps();
+            //{
+            
+           
+
+           /* $table->unsignedBigInteger("utilisateur_id");
+
+            $table->foreign("utilisateur_id")->references("id")->on("utilisateurs");
+            $table->timestamps();*/
+       
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comptes');
+    }
+};
